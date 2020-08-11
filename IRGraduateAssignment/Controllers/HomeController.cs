5f6a6395a4f38ed198e4338f5d60f93a4e33db1e
carrington <carringtonbeard@live.com>
2020-08-11 17:45:58 -0600

    working on building the reader method to return all the status data from capabilites

---
+++ IRGraduateAssignment/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using IRGraduateAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IRGraduateAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int startyear, int endYear)
+        {
+
+            return RedirectToAction("Results");
+
+        }
+
+        public IActionResult Results()
+        {
+            XMLReader.ReadFile();
+            return View();
+        }
+    }
+}
